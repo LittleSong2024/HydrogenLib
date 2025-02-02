f6710e79e50ac4d85ba8e7697e8126d0a3e80d96
LittleSong2024 <idesong6@qq.com>
2025-02-02 21:29:38 +0800

    ci: 更新上传工作流触发条件并优化版本号处理逻辑
    - 在 GitHub Actions 中添加 "v-current-upload*" 标签触发条件- 在脚本中增加对 "current" 版本号的处理逻辑
    - 优化版本号更新流程，只在标签不是 "current" 时更新版本号
    - 重构 run 函数，增加错误处理
    - 调整工作流执行顺序，提前进行版本号检查和更新

--- upload-for-workflow.py
+++ upload-for-workflow.py
@@ -3,8 +3,14 @@ import platform
 import re
 import sys
 
-from subprocess import run, PIPE
+from subprocess import run as _run, PIPE
 from argparse import ArgumentParser
+
+
+def run(*args, **kwargs):
+    if kwargs.get('check') is None:
+        kwargs['check'] = True
+    return _run(*args, **kwargs)
 
 
 class MyParser(ArgumentParser):
@@ -32,17 +38,20 @@ class TagChecker:
     # on:
     #     push:
     #         tags:
+    #             - "v-current-upload*"
     #             - "v*.*.*-upload*"
     #             - "v-patch-upload*"
     #             - "v-major-upload*"
     #             - "v-minor-upload*"
     def __init__(self):
+        self.re_v_current = re.compile(r'v-(current)-upload.*')
         self.re_v_vvv = re.compile(r'v(\d+\.\d+\.\d+)-upload.*')
         self.re_v_patch = re.compile(r'v-(patch)-upload.*')
         self.re_v_major = re.compile(r'v-(major)-upload.*')
         self.re_v_minor = re.compile(r'v-(minor)-upload.*')
 
         self.re_ls = [
+            self.re_v_current,
             self.re_v_vvv,
             self.re_v_patch,
             self.re_v_major,
@@ -62,6 +71,7 @@ class Main:
         self.parser = MyParser()
         self.checker = TagChecker()
         self.version = None
+        self.tag = None
 
     def load_version(self):
         ps = run(['hatch', 'version'], stdout=PIPE)
@@ -70,18 +80,20 @@ class Main:
         self.version = version
 
     def check_tag(self):
-        tag = self.parser.get_tag_name()
+        self.tag = tag = self.parser.get_tag_name()
         if tag:
             # 解析标签
             curv = self.checker.match(tag)
             libv = self.version
             # 如果传入的标签版本号小于当前版本号，则不执行上传
-            if curv not in {'patch', 'major', 'minor'} and tuple(curv.split('.')) < tuple(libv.split('.')):
+            if curv not in {'patch', 'major', 'minor', 'current'} and tuple(curv.split('.')) < tuple(libv.split('.')):
                 print('当前版本号大于传入的版本号，不执行上传')
                 sys.exit(1)  # 阻止工作流继续执行
-
-            print('更新版本号:', curv)
-            self.update_version(curv)
+            if curv == 'current':
+                print('不更新版本号')
+            else:
+                print('更新版本号:', curv)
+                self.update_version(curv)
 
     def update_version(self, ver):
         run(['hatch', 'version', ver])
@@ -94,16 +106,16 @@ class Main:
 
     def exec(self):
 
+        self.load_version()
+        self.check_tag()
+
         print(f'Upload script runs on {platform.platform()}')
         print(f'PID: {os.getpid()}')
         print(f'Args: {sys.argv}')
         print(f'Env: {os.environ}')
-
-        self.load_version()
-
+        print(f'Activity tag: {self.tag}')
         print(f'Get-Version: {self.version}')
 
-        self.check_tag()
         self.build()
         self.upload()
 

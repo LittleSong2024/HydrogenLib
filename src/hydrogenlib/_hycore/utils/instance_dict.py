1802d69762a298fa39dc35e2f55194df9e5228cc
LittleSong2024 <idesong6@qq.com>
2025-03-25 17:15:45 +0800

    refactor(hydrogenlib): - 重构进程管理和字典工具类 - 优化 InstanceDict 类，增加弱引用和自动删除功能

--- src/hydrogenlib/_hycore/utils/instance_dict.py
+++ src/hydrogenlib/_hycore/utils/instance_dict.py
@@ -1,11 +1,24 @@
 from typing import Any, Union
 from collections import UserDict
+import weakref
 
 
 class IDict_Item:
-    def __init__(self, ins, value):
+    def __init__(self, ins, value, parent: 'InstanceDict' = None):
         self.ins = ins
-        self.value = value
+        self._value = weakref.proxy(value, self.delete)
+        self.parent = parent
+
+    def delete(self, object):
+        self.parent.delete(object)
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = weakref.proxy(value, self.delete)
 
 
 class InstanceDict(UserDict):
@@ -15,6 +28,9 @@ class InstanceDict(UserDict):
             for k, v in dct._instances():
                 self._set(k, v)
 
+    def _to_key(self, value):
+        return id(value)
+
     def _get(self, key):
         return super().__getitem__(key)
 
@@ -23,7 +39,7 @@ class InstanceDict(UserDict):
 
     def get(self, k, id_key=False, default=None, item=False) -> Union[IDict_Item, Any]:
         if not id_key:  # 如果 k 不作为 id 传入
-            k = id(k)  # 转换为 id
+            k = self._to_key(k)
 
         if k not in self:  # 如果 k 不位于字典中
             return default  # 返回默认值
@@ -37,22 +53,22 @@ class InstanceDict(UserDict):
         self._set(k, v)
 
     def delete(self, key):
-        del self[id(key)]
+        del self[self._to_key(key)]
 
     def pop(self, key, id_key=False):
         if not id_key:
-            key = id(key)
+            key = self._to_key(key)
 
         return super().pop(key)
 
     def __getitem__(self, key):
-        return self._get(id(key))
+        return self._get(self._to_key(key))
 
     def __setitem__(self, key, value):
         self._set(key, value)
 
     def __delitem__(self, key):
-        super().__delitem__(id(key))
+        super().__delitem__(self._to_key(key))
         
     def __contains__(self, item):
-        return super().__contains__(id(item))
+        return super().__contains__(self._to_key(item))

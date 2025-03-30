4aed91dd362ea8e933ff2aab9c656ee2d4399748
LittleSong2024 <idesong6@qq.com>
2025-03-30 17:45:00 +0800

    feat(HyStructure): 增加结构体的字符串表示和排序功能
    - 为 HyStructure 类添加了 __str__ 和 __repr__ 方法，使其能够以字符串形式显示结构体内容
    - 添加了 _length_ 属性和相关的格式化逻辑，用于对齐结构体字段
    - 修改了 __init_subclass__ 方法，支持设置字段顺序并自动计算最大字段长度

--- src/hydrogenlib/_hyctypes/universality.py
+++ src/hydrogenlib/_hyctypes/universality.py
@@ -9,30 +9,50 @@ def get_annotation(cls, name):
         return get_annotation(base, name)
 
 
-#
-#
+def _c__str__(self):
+    msg = f"struct {self.__class__.__name__}: \n"
+    field_fmt = f'<{self._length_}'
+    length = 0
+    for field, type in self._fields_:
+        value = str(getattr(self, field))
+        length = max(len(value), length)
+
+        value_fmt = f'<{length}'
+
+        msg += f"\t{format(field, field_fmt)} = {format(value, value_fmt)}  # type: {type.__name__}\n"
+    return msg
+
+
 class HyStructure(ctypes.Structure):
     _fields_ = []
     _order_ = None
+    _length_ = 0
 
     c_struct = None
 
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
+        max_length = cls._length_
         if cls._order_:
             fields = []
             for field in cls._order_:
+                max_length = max(max_length, len(field))
                 fields.append((field, get_annotation(cls, field)))
             cls._fields_ = fields
 
         else:
             cls._fields_.extend([*map(tuple, cls.__annotations__.items())])
+            for field, _ in cls.__annotations__.items():
+                max_length = max(max_length, len(field))
 
         cls.c_struct = type(
             cls.__name__,
             (ctypes.Structure,),
             {
-                '_fields_': cls._fields_
+                '_fields_': cls._fields_,
+                '_length_': max_length,
+                '__str__': _c__str__,
+                '__repr__': _c__str__
             }
         )
         # cls._type_ = cls.__structure__._type_
@@ -42,4 +62,3 @@ class HyStructure(ctypes.Structure):
         for k, v in kwargs.items():
             setattr(self, k, v)
         return self
-

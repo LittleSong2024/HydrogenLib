7d9d10b0f9f8c531d705ba32c9803fcaf8476e40
LittleSong2024 <idesong6@qq.com>
2025-04-15 18:48:25 +0800

    refactor(hydrogenlib):重构字典相关函数和模板匹配逻辑
    - 重命名 dict_func.py 中的函数，使其命名更加准确和一致
    - 更新 template_type.py 中的模板匹配和排序逻辑
    - 优化代码结构，提高可读性和可维护性

--- src/hydrogenlib/_hycore/type_func/tempalte_type.py
+++ src/hydrogenlib/_hycore/type_func/tempalte_type.py
@@ -1,21 +1,21 @@
 class Template:
-    def __init__(self, **tem):
-        self._template = tem
+    def __init__(self, **dct):
+        self._template = dct
 
     @property
     def template(self):
         return self._template.copy()
 
 
-def match(dic, tem):
+def template_match(dic, template):
     from . import dict_func as dictx
-    if dictx.sub(tem, dic) and dictx.parent(tem, dic):
+    if dictx.is_sub_dict(template, dic) and dictx.is_parent_dict(template, dic):
         return True
     else:
         return False
 
 
-def sort(dic, tem):
+def template_sort(dic, tem):
     # 修改字典使字典符合模板
     for i in tem.template:
         if i not in dic:
@@ -23,8 +23,8 @@ def sort(dic, tem):
     return dic
 
 
-def sub(template: Template, data: dict):
+def template_sub(template: Template, data: dict):
     if data is None:
         return False
-    from . import dict_func as dictx
-    return dictx.sub(template.template, data)
+    from . import dict_func
+    return dict_func.is_sub_dict(template.template, data)

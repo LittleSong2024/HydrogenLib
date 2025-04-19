260b2c1a0da70741256b969563a24e86c976d0ff
LittleSong2024 <idesong6@qq.com>
2025-04-19 20:31:37 +0800

    feat(GettingPath): 添加设置对象和获取父对象的功能
    - 新增 parent 属性，用于获取当前路径的父对象
    - 新增 name 属性，用于获取当前路径的名称
    - 新增 setnext 方法，用于设置当前路径中的下一个对象
    - 新增 set 方法，用于将对象设置到指定路径

--- src/hydrogenlib/_hycore/utils/getting_path.py
+++ src/hydrogenlib/_hycore/utils/getting_path.py
@@ -5,6 +5,20 @@ class GettingPath:
 
         self.path = path
 
+    @property
+    def parent(self):
+        """
+        Get parent object
+        """
+        return GettingPath(self.path[:-1])
+
+    @property
+    def name(self):
+        """
+        Get name of object
+        """
+        return self.path[-1]
+
     def check(self, path):
         """
         Check if path is valid
@@ -17,6 +31,12 @@ class GettingPath:
         """
         return getattr(current, next)
 
+    def setnext(self, current, next, value):
+        """
+        Set next object from current object
+        """
+        setattr(current, next, value)
+
     def iter_path(self):
         return self.path
 
@@ -25,3 +45,9 @@ class GettingPath:
         for next in self.iter_path():
             cur = self.getnext(cur, next)
         return cur
+
+    def set(self, obj):
+        cur = obj
+        for next in self.iter_path()[:-1:]:
+            cur = self.getnext(cur, next)
+        self.setnext(cur, self.iter_path()[-1], obj)

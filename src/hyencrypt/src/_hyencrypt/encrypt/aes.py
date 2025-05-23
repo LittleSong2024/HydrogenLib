057d5f52a8cb18389152c4d63acd9254f0d26c9d
LittleSong2024 <idesong6@qq.com>
2025-05-23 21:23:20 +0800

    refactor(aes): 重构 AES 加密模块
    - 移除了不必要的导入和未使用的代码
    - 添加了类型注解以提高代码可读性
    - 重构了 AES 加密和解密逻辑，使用类封装
    - 增加了对不同 AES 模式的支持
    - 优化了代码结构，提高了模块的可维护性和扩展性

--- src/hyencrypt/src/_hyencrypt/encrypt/aes.py
+++ src/hyencrypt/src/_hyencrypt/encrypt/aes.py
@@ -1,103 +1,90 @@
-import logging
 import os
-from typing import Iterable
+from enum import Enum
 
 import pyaes
 
 from .methods import split, pad, unpad
 
-zencrypt_logger = logging.getLogger("SzQlib.zencrypt")
 
-
-# module end
-
-
-def aes_encrypt(plaintext: bytes, key, iv=None):
-    if iv is None:
-        iv = os.urandom(16)
-
-    plaintext_bytes = plaintext
-    padded_plaintext = plaintext_bytes
-    aes = pyaes.AESModeOfOperationCBC(key, iv)
-    ciphertext = aes.encrypt(padded_plaintext)
-    return ciphertext, iv
-
-
-def aes_decrypt(ciphertext, key, iv):
-    aes = pyaes.AESModeOfOperationCBC(key, iv)
-    decrypted_data = aes.decrypt(ciphertext)
-    return decrypted_data
-
-
-def aes_encrypt_ls(plain_text_list: Iterable, key, vi=None, NoIV=False):
-    for i in plain_text_list:
-        if NoIV:
-            yield aes_encrypt(i, key, vi)[0]
-        else:
-            yield aes_encrypt(i, key, vi)
-
-
-def aes_detrypt_ls(cipher_text_list, key, vi):
-    for i in cipher_text_list:
-        yield aes_decrypt(i, key, vi)
-
-
-def generate_key(size=16):
+def generate_key(size: int = 16):
     return os.urandom(size)
 
 
-def generate_iv(size=16):
+def generate_iv(size: int = 16):
     return os.urandom(size)
 
 
-def generate(size=16):
+def generate(size: int = 16):
     return generate_key(size), generate_iv(size)
 
 
-def split_pad(text: bytes, size=16):
+def split_and_pad(text: bytes, size: int = 16):
     return [pad(i, size) for i in split(text, size)]
 
 
-def join(args):
+def bytes_join(args):
     return b''.join(args)
 
 
-def encode(text: bytes, key, iv):  # 提供一个最简单的加密api
-    """
-    加密函数，提供一个最简单的api加快使用效率
-    :param text:
-    :param key:
-    :param iv:
-    :return: bytes
-    """
-    byte = text
-    byte = split_pad(byte)
-    crypt_text = aes_encrypt_ls(byte, key, iv, NoIV=True)
-
-    return join(crypt_text)
+class AESMode(Enum):
+    CBC = pyaes.AESModeOfOperationCBC
+    ECB = pyaes.AESModeOfOperationECB
+    CFB = pyaes.AESModeOfOperationCFB
+    OFB = pyaes.AESModeOfOperationOFB
+    CTR = pyaes.AESModeOfOperationCTR
 
 
-def decode(crypt_text: bytes, key, iv):  # 提供一个最简单的解密api
-    """
-    解密，提供一个最简单的api加快使用效率
-    :param crypt_text:
-    :param key:
-    :param iv:
-    :return: str
-    """
-    crypt_list = split(crypt_text)
-    plain_text = list(aes_detrypt_ls(crypt_list, key, iv))
-    for i in range(len(plain_text)):
-        plain_text[i] = unpad(plain_text[i])
-    return join(plain_text)
+class Aes:
+    @property
+    def key(self):
+        return self._key
 
+    @property
+    def iv(self):
+        return self._iv
 
-def encrypt(plain_text: bytes, key, iv=None):  # 别名
-    return encode(plain_text, key, iv)
+    @key.setter
+    def key(self, key):
+        self._key = key
+        self._generate_aes_object()
 
+    @iv.setter
+    def iv(self, iv):
+        self._iv = iv
+        self._generate_aes_object()
 
-def decrypt(crypt_text: bytes, key, iv=None):  # 别名
-    return decode(crypt_text, key, iv)
+    def _generate_aes_object(self):
+        self._aes = self._type.value(self.key, self.iv)
 
+    def __init__(self, key, iv, type=AESMode.CBC):
+        self._key = key
+        self._iv = iv
+        self._type = type
+        self._aes = None
 
-zencrypt_logger.debug("Module zencrypt loading ...")
+        self._generate_aes_object()
+
+    def decrypt(self, ciphertext: bytes) -> bytes:
+        """
+        解密
+        :param ciphertext: 原始密文
+        :return: bytes
+        """
+        ciphertext_list = split(ciphertext)
+        plaintext_list = [self._aes.decrypt(i) for i in ciphertext_list]
+
+        if plaintext_list:
+            plaintext_list[-1] = unpad(plaintext_list[-1])
+
+        return bytes_join(plaintext_list)
+
+    def encrypt(self, text: bytes):
+        """
+        加密
+        :param text: 明文
+        :return: bytes
+        """
+        plaintext_list = split_and_pad(text)
+        cipertext_list = [self._aes.encrypt(i) for i in plaintext_list]
+
+        return bytes_join(cipertext_list)

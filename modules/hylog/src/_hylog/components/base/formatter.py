e2d36c5476fa750cefbcc93de8ecd9818ce6fdf5
LittleSong2024 <idesong6@qq.com>
2025-06-13 21:53:53 +0800

    build: hylog 模块

--- modules/hylog/src/_hylog/components/base/formatter.py
+++
@@ -1,6 +0,0 @@
-from .abstracts import AbstractFormatter, LoggingData
-
-
-class Formatter(AbstractFormatter):
-    def format(self, data: LoggingData):
-        return f'{data.timestamp}: [{data.level}] {data.message}'
--- modules/hylog/src/_hylog/components/base/handler.py
+++
@@ -1,6 +0,0 @@
-from .abstracts import AbstractHandler, LoggingData
-
-
-class Handler(AbstractHandler):
-    def handle(self, message, data: LoggingData):
-        print(message)

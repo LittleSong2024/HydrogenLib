ac83a9897d52e2b095afb13c3891814af343e4ca
LittleSong2024 <idesong6@qq.com>
2025-07-10 15:14:21 +0800

    refactor(typefunc.property): 更新属性别名类的文档说明

--- modules/hycore/src/_hycore/typefunc/property.py
+++ modules/hycore/src/_hycore/typefunc/property.py
@@ -11,7 +11,7 @@ class aliasmode(int, _enum.Enum):
 
 class alias:
     """
-    声明变量别名
+    声明属性别名
 
     class myclass:
         a = ...
@@ -19,6 +19,8 @@ class alias:
         b = alias['a'](mode=alias.mode.read)  # 这将创建一个只读的别名
         c = alias['a'](mode=alias.mode.write)  # 这将创建一个只写的别名
         d = alias['a'](mode=alias.mode.read_write)  # 这将创建一个可同时读写的别名
+
+        # 当然这些声明方式都可以用原始的 alias(name, mode, classvar_enabled) 来代替
 
     你也可以自定义钩子
 
@@ -38,6 +40,31 @@ class alias:
         def a_alias(self):
             # raise AttributeError(...)  # 如果你想中断 delete 操作,你只能通过抛出异常的方式
             pass
+
+
+    是不是觉得这些功能太简单了
+    实际上,属性别名可以应用到嵌套的属性中
+    比如:
+
+    class myclass:
+        a = otherclass()
+        b = alias['a.xxx.xxx'](mode=alias.mode.read_write)
+
+    这时候对 b 操作
+
+    obj = myclass()
+    obj.b  # 等同于 obj.a.xxx.xxx
+    obj.b = ... # 等同于 obj.a.xxx.xxx = ...
+
+    别名也可以应用在 class var 上,这要求开启 classvar_enabled, 但因为 Python 描述符本身的限制, class var 模式仅可读
+    当你尝试以 class var 的形式为一个 alias 赋值时,会发生与你预期不同的事: alias 对象被覆盖
+    如果不开启 classvar_enabled 那么任何以 class var 形式获取的别名都将返回 alias 对象本身
+
+    class myclass:
+        a = ...
+        b = alias['a'](mode=alias.mode.read, classvar_enabled=True)
+
+    myclass.b  # 等同于 myclass.a
 
     """
     mode = aliasmode

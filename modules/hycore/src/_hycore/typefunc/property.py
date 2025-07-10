ded7ea35b25662efbebabde50ec05ca7e5cf69c7
LittleSong2024 <idesong6@qq.com>
2025-07-10 14:45:24 +0800

    refactor(hycore): 重构 typefunc 模块
    - 将 __init__.py 中的方法移动到了 common.py
    - 优化了模块结构，提高了代码的可维护性和可访问性
    - 添加了 alias 类，用于创建属性别名

---
+++ modules/hycore/src/_hycore/typefunc/property.py
@@ -0,0 +1,52 @@
+from . import get_attr_by_path as _gabp, set_attr_by_path as _sabp, del_attr_by_path as _dabp
+import enum as _enum
+from typing import Callable, Any
+
+
+class aliasmode(int, _enum.Enum):
+    read = 0
+    write = 1
+    read_write = 2
+
+
+class alias:
+    mode = aliasmode
+
+    _get = lambda self, x: x
+    _set = lambda self, v: v
+    _del = lambda self: None
+
+    def __init__(self, attr_path, mode=aliasmode.read, classvar_enabled=False):
+        self.path = attr_path
+        self.mode = mode
+        self.cve = classvar_enabled
+
+    def getter(self, fnc: Callable[[Any, Any], Any]):
+        self._get = fnc
+
+    def setter(self, fnc: Callable[[Any, Any], Any]):
+        self._set = fnc
+
+    def deleter(self, fnc: Callable[[Any], Any]):
+        self._del = fnc
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            if self.cve:
+                instance = owner
+            else:
+                return self
+        if self.mode == aliasmode.read:
+            return self._get(instance, _gabp(instance, self.path))
+        raise PermissionError("Can't read alias")
+
+    def __set__(self, instance, value):
+        if self.mode == aliasmode.write:
+            _sabp(instance, self.path, self._set(instance, value))
+        raise PermissionError("Can't write alias")
+
+    def __delete__(self, instance):
+        if self.mode == aliasmode.write:
+            self._del(instance)
+            _dabp(instance, self.path)
+        raise PermissionError("Can't delete alias")
